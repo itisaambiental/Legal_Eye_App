2b269734507ca96b6581e57fdf9dfc75ddead491
Alfredo <alfredocastellanoula.14@gmail.com>
2024-11-18 15:41:05 -0600

    Fix Update user picture bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,17 +33,6 @@ function Home() {
   return (
     <section className="bg-gray-50 mt-12 text-center justify-center  flex flex-col items-center -ml-64">
       <div className="py-8 px-4 max-w-screen-xl text-center lg:py-16 z-10 relative container xs:mx-auto sm:mx-auto md:mx-auto lg:mx-36 xl:mx-64 lg:pl-80">
-        <button
-          onClick={handleDynamicClick}
-          className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-primary bg-secondary/30 rounded-full dark:bg-primary-900 dark:text-primary-300 hover:bg-primary/40 sm:text-xs md:text-sm"
-        >
-          <span className="text-xs bg-secondary rounded-full text-white px-4 py-1.5 me-3">¡Confía en ti!</span>
-          <span className="text-sm font-medium">Empieza a trabajar ahora.</span>
-          <svg className="w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-          </svg>
-        </button>
-
         <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-primary dark:text-white sm:text-2xl md:text-5xl lg:text-6xl">Bienvenido {name || "Invitado"}</h1>
         <p className="mb-8 text-lg font-normal text-secondary lg:text-xl sm:px-12 lg:px-44 sm:text-sm md:text-base">Dale click aca abajo para empezar</p>
         <form className="w-full max-w-md mx-auto sm:w-full md:w-3/4 lg:w-1/2">

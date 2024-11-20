6ff0a343914d047787c8a25a767b33c5c9778907
itisaambiental <it@isaambiental.com>
2024-11-20 10:46:26 -0600

    Home Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,7 +41,7 @@ function Home() {
               onClick={handleDynamicClick}
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary/80 sm:text-sm md:text-base"
             >
-              Comenzar
+              Comenzar ahora
               <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
               </svg>

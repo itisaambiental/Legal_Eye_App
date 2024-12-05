b636006a2a798e2c3e6e07920cb1f95affb2f808
itisaambiental <it@isaambiental.com>
2024-12-05 17:40:03 -0600

    Refactoring Frotnend App

--- src/components/utils/Error.test.jsx
+++ src/components/utils/Error.test.jsx
@@ -1,28 +1,41 @@
 /* eslint-disable no-undef */
 import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom"; // Importa MemoryRouter
 import Error from "./Error.jsx";
 
 describe("Error Component", () => {
+  const renderWithRouter = (ui) => {
+    return render(<MemoryRouter>{ui}</MemoryRouter>);
+  };
+
   test("displays network error message correctly", () => {
-    render(<Error title="Error de conexión" message="Revisa tu conexión a Internet e intenta de nuevo." />);
+    renderWithRouter(
+      <Error title="Error de conexión" message="Revisa tu conexión a Internet e intenta de nuevo." />
+    );
     expect(screen.getByText("Error de conexión")).toBeInTheDocument();
     expect(screen.getByText("Revisa tu conexión a Internet e intenta de nuevo.")).toBeInTheDocument();
   });
 
   test("displays unauthorized access message correctly", () => {
-    render(<Error title="Acceso no autorizado" message="Asegúrate de tener los permisos necesarios." />);
+    renderWithRouter(
+      <Error title="Acceso no autorizado" message="Asegúrate de tener los permisos necesarios." />
+    );
     expect(screen.getByText("Acceso no autorizado")).toBeInTheDocument();
     expect(screen.getByText("Asegúrate de tener los permisos necesarios.")).toBeInTheDocument();
   });
 
   test("displays server error message correctly", () => {
-    render(<Error title="Error en el servidor" message="Por favor, intenta nuevamente más tarde." />);
+    renderWithRouter(
+      <Error title="Error en el servidor" message="Por favor, intenta nuevamente más tarde." />
+    );
     expect(screen.getByText("Error en el servidor")).toBeInTheDocument();
     expect(screen.getByText("Por favor, intenta nuevamente más tarde.")).toBeInTheDocument();
   });
 
   test("displays default error message correctly", () => {
-    render(<Error title="Algo mal sucedió" message="Por favor, espere un momento e intenta nuevamente." />);
+    renderWithRouter(
+      <Error title="Algo mal sucedió" message="Por favor, espere un momento e intenta nuevamente." />
+    );
     expect(screen.getByText("Algo mal sucedió")).toBeInTheDocument();
     expect(screen.getByText("Por favor, espere un momento e intenta nuevamente.")).toBeInTheDocument();
   });
@@ -31,10 +44,13 @@ describe("Error Component", () => {
     const originalLocation = window.location;
     delete window.location;
     window.location = { reload: vi.fn() };
-    render(<Error title="Error de conexión" message="Revisa tu conexión a Internet e intenta de nuevo." />);
+
+    renderWithRouter(
+      <Error title="Error de conexión" message="Revisa tu conexión a Internet e intenta de nuevo." />
+    );
     const retryButton = screen.getByText("Intentar de nuevo");
     fireEvent.click(retryButton);
-  
+
     expect(window.location.reload).toHaveBeenCalled();
     window.location = originalLocation;
   });

6f43b973b98e67fb562b5dfc004d4b3d91e85fa9
itisaambiental <it@isaambiental.com>
2025-01-06 15:26:23 -0600

    Testing Update Legal Basis Funcionality

--- src/components/legalBasis/CreateModal.jsx
+++ src/components/legalBasis/CreateModal.jsx
@@ -393,7 +393,7 @@ function CreateModal({
                       value={formData.abbreviation}
                       onChange={handleAbbreviationChange}
                       className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
-                      placeholder="Abreviatura"
+                      placeholder=""
                     />
                     <label
                       htmlFor="floating_abbreviation"

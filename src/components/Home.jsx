39511f7ffc2a828ed91a27b6747de224c2c228c9
itisaambiental <it@isaambiental.com>
2025-01-29 12:47:39 -0600

    Improving Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import useUserProfile from "../hooks/user/useUserProfile.jsx";
+import { Button } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import Error from "./utils/Error.jsx";
 import { Spinner } from "@nextui-org/react";
+import search_icon from "../assets/busqueda_blue.png";
+import time_icon from "../assets/time.png";
+import play_icon from "../assets/play.png";
+
 
 /**
  * Home component
@@ -10,7 +15,7 @@ import { Spinner } from "@nextui-org/react";
  * @returns {JSX.Element} The rendered Home component.
  */
 function Home() {
-  const { name, loading, error } = useUserProfile();
+  const { loading, error } = useUserProfile();
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -36,42 +41,135 @@ function Home() {
   }
 
   return (
-    <section className="bg-gray-50 mt-12 text-center justify-center  flex flex-col items-center -ml-64">
-      <div className="py-8 px-4 max-w-screen-xl text-center lg:py-16 z-10 relative container xs:mx-auto sm:mx-auto md:mx-auto lg:mx-36 xl:mx-64 lg:pl-80">
-        <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-primary dark:text-white sm:text-2xl md:text-5xl lg:text-6xl">
-          Bienvenido {name || "Invitado"}
-        </h1>
-        <p className="mb-8 text-lg font-normal text-secondary lg:text-xl sm:px-12 lg:px-44 sm:text-sm md:text-base">
-          Dale click aca abajo para empezar
-        </p>
-        <form className="w-full max-w-md mx-auto sm:w-full md:w-3/4 lg:w-1/2">
-          <div className="relative">
-            <button
-              onClick={handleClick}
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary/80 sm:text-sm md:text-base"
-            >
-              Comenzar ahora
-              <svg
-                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+    <div className="flex flex-col min-h-screen">
+      <section className="flex-grow bg-gray-50 mt-12 text-center flex flex-col items-center -ml-64 px-4 sm:px-6 lg:px-8">
+        <div className="py-8 px-4 max-w-screen-xl text-center lg:py-16 z-10 relative container xs:mx-auto sm:mx-auto md:mx-auto lg:mx-36 xl:mx-64 lg:pl-80">
+          <div className="bg-gray-50 border border-gray-200 rounded-lg p-8 md:p-12 mb-8">
+          <Button
+                onPress={handleClick}
+                className="-mt-8 mb-4"
+                size="sm"
+                variant="flat"
+                radius="full"
+                startContent={
+                  <img
+                    src={play_icon}
+                    alt="Search Icon"
+                    className="w-4 h-4 flex-shrink-0"
+                  />
+                }
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </button>
+                <span className="text-primary">Comienza</span>
+              </Button>
+            <h1 className="text-primary text-4xl font-extrabold mb-2">
+              Comienza a analizar fundamentos legales
+            </h1>
+            <p className="text-lg font-normal text-gray-500 mb-8">
+              Analiza fundamentos legales de manera instantánea. Obtén
+              respuestas precisas usando IA y ahorra tiempo en tu flujo de
+              trabajo.
+            </p>
+            <Button onPress={handleClick} color="primary">Pruébalo ahora</Button>
           </div>
-        </form>
-      </div>
-      <div className="bg-gray-50  dark:from-primary-900 w-full h-full absolute top-0 left-0 z-0"></div>
-    </section>
+          <div className="grid md:grid-cols-2 gap-8">
+            <div className="bg-gray-50 border border-gray-200 rounded-lg p-8 md:p-12">
+              <Button
+                onPress={handleClick}
+                className="-mt-8 mb-4"
+                size="sm"
+                variant="flat"
+                radius="full"
+                startContent={
+                  <img
+                    src={search_icon}
+                    alt="Search Icon"
+                    className="w-4 h-4 flex-shrink-0"
+                  />
+                }
+              >
+                <span className="text-primary">Analizar</span>
+              </Button>
+              <h2 className="text-gray-900 text-3xl font-extrabold mb-2">
+                Análisis legal en segundos
+              </h2>
+              <p className="text-lg font-normal text-gray-500 mb-4">
+                Ingresa cualquier documento y obtén un análisis automático con
+                sus artículos relevantes.
+              </p>
+              <a
+                onClick={handleClick}
+                className="text-primary hover:underline font-medium text-lg inline-flex items-center"
+              >
+                Hazlo ahora
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+
+            <div className="bg-gray-50 border border-gray-200 rounded-lg p-8 md:p-12">
+              <Button
+                onPress={handleClick}
+                className="-mt-8 mb-4"
+                size="sm"
+                variant="flat"
+                radius="full"
+                startContent={
+                  <img
+                    src={time_icon}
+                    alt="Search Icon"
+                    className="w-4 h-4 flex-shrink-0"
+                  />
+                }
+              >
+                <span className="text-primary">Ahorrar</span>
+              </Button>
+              <h2 className="text-gray-900 text-3xl font-extrabold mb-2">
+                Ahorra tiempo y esfuerzo
+              </h2>
+              <p className="text-lg font-normal text-gray-500 mb-4">
+                Evite búsquedas largas. Nuestro objetivo es facilitarle el
+                trabajo para que solo tenga que revisar los resultados clave.
+              </p>
+              <a
+                onClick={handleClick}
+                className="text-primary hover:underline font-medium text-lg inline-flex items-center"
+              >
+                Aprende más
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 w-full h-full absolute top-0 left-0 z-0"></div>
+      </section>
+    </div>
   );
 }
 
